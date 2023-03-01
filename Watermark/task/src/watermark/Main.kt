48469fe5664640b5658b612b8b408635eb001fee
Epap <peia@inbox.ru>
2023-03-01 21:59:31 +0300

    Stage 3/5: Blending images with an alpha channel

--- Watermark/task/src/watermark/Main.kt
+++ Watermark/task/src/watermark/Main.kt
@@ -1,6 +1,7 @@
 package watermark
 
 import java.awt.Color
+import java.awt.Transparency
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
@@ -64,6 +65,23 @@ fun blendingImages(image: BufferedImage, watermarkImage: BufferedImage, transpar
     return watermarkedImage
 }
 
+fun blendingImagesUsingAlpha(image: BufferedImage, watermarkImage: BufferedImage, transparency: Int): BufferedImage {
+    val watermarkedImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
+    for (x in 0 until image.width) {
+        for (y in 0 until image.height) {
+            val iColor = Color(image.getRGB(x, y))
+            val wColor = Color(watermarkImage.getRGB(x, y), true)
+            val color = Color(
+                (wColor.red * transparency * wColor.alpha / 255 + iColor.red * (100 - transparency * wColor.alpha / 255)) / 100,
+                (wColor.green * transparency * wColor.alpha / 255 + iColor.green * (100 - transparency * wColor.alpha / 255)) / 100,
+                (wColor.blue * transparency * wColor.alpha / 255 + iColor.blue * (100 - transparency * wColor.alpha / 255)) / 100
+            )
+            watermarkedImage.setRGB(x, y, color.rgb)
+        }
+    }
+    return watermarkedImage
+}
+
 fun writeImageFile(outputImage: BufferedImage) {
     println("Input the output image filename (jpg or png extension):")
     val fileName = readln()
@@ -73,13 +91,28 @@ fun writeImageFile(outputImage: BufferedImage) {
     println("The watermarked image $fileName has been created.")
 }
 
+fun checkUsingOfAlpha(watermarkImage: BufferedImage): Boolean {
+    var usingOfAlpha = false
+
+    if (watermarkImage.transparency == Transparency.TRANSLUCENT) {
+        println("Do you want to use the watermark's Alpha channel?")
+        if (readln().lowercase() == "yes") {
+            usingOfAlpha = true
+        }
+    }
+
+    return usingOfAlpha
+}
+
 fun watermark() {
     try {
         val image = inputFileImage("")
         val watermarkImage = inputFileImage("watermark ")
         checkImages(image, watermarkImage)
+        val usingAlpha = checkUsingOfAlpha(watermarkImage)
         val transparency = readTransparencyPercentage()
-        val watermarkedImage = blendingImages(image, watermarkImage, transparency)
+        val watermarkedImage = if (usingAlpha) blendingImagesUsingAlpha(image, watermarkImage, transparency)
+        else blendingImages(image, watermarkImage, transparency)
         writeImageFile(watermarkedImage)
 
     } catch (e: Exception) {
