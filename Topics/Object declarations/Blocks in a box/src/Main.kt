10feda16ba3096047ee7a936d5cf551d13046e31
Epap <peia@inbox.ru>
2023-03-06 18:07:01 +0300

    Object declaration

---
+++ Topics/Object declarations/Blocks in a box/src/Main.kt
@@ -0,0 +1,14 @@
+@SuppressWarnings("MagicNumber")
+data class Block(val color: String) {
+
+    object BlockProperties {
+
+        var length = 6
+        var width = 4
+
+        fun blocksInBox(length: Int, width: Int): Int {
+            return length / BlockProperties.length * (width / BlockProperties.width)
+
+        }
+    }
+}
---
+++ Topics/Object declarations/Library/src/Main.kt
@@ -0,0 +1,7 @@
+object Math {
+
+    fun abs(number: Int) = if (number >= 0) number else -number
+
+    fun abs(number: Double) = if (number >= 0) number else -number
+
+}
