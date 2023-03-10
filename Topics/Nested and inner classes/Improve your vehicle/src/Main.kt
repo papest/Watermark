6f80490211d12c5ede36ad7bdba684f63d1b7487
Epap <peia@inbox.ru>
2023-03-10 12:00:05 +0300

    Another task with an inner class

---
+++ Topics/Nested and inner classes/Improve your vehicle/src/Main.kt
@@ -0,0 +1,11 @@
+data class Vehicle(val name: String) {
+    inner class Engine(val horsePower: Int) {
+        fun start() {
+            println("RRRrrrrrrr....")
+        }
+
+        fun printHorsePower() {
+            println("The $name vehicle has $horsePower horsepower.")
+        }
+    }
+}
